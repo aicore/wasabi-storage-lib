f4b6348ea2c1b6b96f030735202e01a25c178e21
ushmajit <ushmajit@gmail.com>
2022-02-14 18:39:48 +0530

    Added Wasabi Storage Helper module

---
+++ src/aws_s3_client_module.js
@@ -0,0 +1,97 @@
+
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+import pkg from 'aws-sdk';
+import joinURL from 'url-join';
+import path from 'path';
+import fsExtra from "fs-extra";
+import stream from "stream";
+
+const {S3, Endpoint} = pkg;
+
+/**
+ * Module to upload a file to AWS S3/Linode Object Storage. The path of the file to be uploaded is
+ * passed on as a parameter which is then processed and uploaded using AWS S3 client.
+ *
+ * @param accessKeyId bucket specific unique identifier required for authentication
+ * @param secretAccessKey user specific unique identifier required for authentication
+ * @param region indicates the geographical server location (e.g us-east-1, eu-west-1a)
+ * @param file complete path of the file to be uploaded is passed on as a parameter
+ * @param bucket uniquely identifies the bucket where the file should be uploaded
+ * @param url suffix url to decide whether to upload the file to AWS S3 or LiNode Object Storage
+ * @returns {Promise<void>} void
+ */
+
+async function uploadFileToBucket (accessKeyId, secretAccessKey, region, file, bucket, url) {
+    const uploadStream = ({ Bucket, Key }) => {
+        const s3Client = new S3({
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+            endpoint: new Endpoint('https://s3.'+ region + url)
+        });
+        const pass = new stream.PassThrough();
+        return {
+            writeStream: pass,
+            promise: s3Client.upload({ Bucket, Key, Body: pass }).promise()
+        };
+    };
+    const filePath = path.basename(file);
+    const { writeStream, promise } = uploadStream({Bucket: bucket, Key: filePath});
+    fsExtra.createReadStream(filePath).pipe(writeStream);
+    const response = {
+        status: true,
+        errorMessage: ''
+    };
+    await promise.catch((reason)=> {
+        response.status = false;
+        response.errorMessage = reason.toString();
+    });
+    return response;
+}
+
+/**
+ * 
+ * @param region 
+ * @param bucketName 
+ * @param objectName 
+ * @returns getObjectResponse
+ */
+async function getObject(accessKeyId, secretAccessKey, region, bucketName, objectName, url) {
+    try {
+        const s3Client = new S3({
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+            endpoint: new Endpoint('https://s3.'+ region + url
+            )
+        });
+        
+        let params = {
+            Bucket: bucketName,
+            Key: objectName,
+        };
+        
+       return await s3Client.getObject(params).promise();
+    } catch (e) {
+        throw new Error(`Could not retrieve file from bucket: ${e.message}`);
+    }
+}
+
+export default {
+    uploadFileToBucket,
+    getObject
+};
---
+++ src/wasabi_storage_module.js
@@ -0,0 +1,64 @@
+/*
+ * GNU AGPL-3.0 License
+ *
+ * Copyright (c) 2021 - present core.ai . All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see https://opensource.org/licenses/AGPL-3.0.
+ *
+ */
+import s3Client from './aws_s3_client_module.js';
+
+const BASE_LINODE_URL_SUFFIX = '.wasabisys.com';
+
+/**
+ * Wasabi Storage Helper Module to upload a file to object storage. The file is uploaded using
+ * AWS S3 Client. Please refer to https://docs.aws.amazon.com/sdk-for-javascript/index.html for more details.
+ * 
+ * @param accessKeyId bucket specific unique identifier required for authentication
+ * @param secretAccessKey user specific unique identifier required for authentication
+ * @param region indicates the geographical server location (e.g us-east-1, eu-west-1a)
+ * @param file complete path of the file to be uploaded is passed on as a parameter
+ * @param bucket uniquely identifies the bucket where the file should be uploaded
+ * @returns {Promise<void>}
+ */
+ async function uploadFileToBucket(accessKeyId, secretAccessKey, region, file, bucket) {
+    if (!accessKeyId || !secretAccessKey || !region || !file || !bucket) {
+        throw new Error("Invalid parameter value: accessKeyId, secretAccessKey, region, filePath " +
+        "and bucketName are required parameters");
+    }
+
+    const response = await s3Client.uploadFileToBucket(accessKeyId,
+        secretAccessKey, region, file, bucket, BASE_LINODE_URL_SUFFIX);
+    console.log("Object Upload Response : " + JSON.stringify(response));
+    return response;
+}
+
+async function fetchObject(accessKeyId, secretAccessKey, region, bucketName, objectName) {
+    if (!region || !bucketName || !objectName) {
+        throw new Error("Invalid parameter value: accessToken, region, fileName " +
+        "and bucketName are required parameters");
+    }
+    const response = await s3Client.getObject(accessKeyId, secretAccessKey, region, 
+        bucketName, objectName, BASE_LINODE_URL_SUFFIX);
+    
+    if (!response || !response.Body || !response.Body.data) {
+        throw new Error("Invalid Response: Body or Body.Data is missing");
+    }    
+
+    console.log("Object Fetch Response : " + JSON.stringify(body));
+    return body.Body.data;
+}
+
+export default {
+    uploadFileToBucket,
+    fetchObject
+};
